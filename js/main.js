ff6fc03187714d8744b8374885f45c94f1be492d
Toph Tucker <tophtucker@gmail.com>
2014-01-27 19:41:07 -0500

    adding dummy annotation line

--- js/main.js
+++ js/main.js
@@ -108,6 +108,17 @@ function init(){
 	mesh.name = "tail";
 	scene.add( mesh );
 	
+	
+	//annotations line
+	var material = new THREE.LineBasicMaterial({
+        color: 0x000000
+    });
+	var geometry = new THREE.Geometry();
+    geometry.vertices.push(new THREE.Vector3(-10, 0, 50));
+    geometry.vertices.push(new THREE.Vector3(10, 0, 0));
+    var line = new THREE.Line(geometry, material);
+    scene.add(line);	
+	
 	// Create lights
 	var light = new THREE.PointLight(0xEEEEEE);
 	light.position.set(-20, 0, 20);
